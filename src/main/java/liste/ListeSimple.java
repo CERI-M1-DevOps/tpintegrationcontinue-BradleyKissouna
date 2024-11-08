56a741c86091ba22e1f278d3bc8b0f998b0fdf1e
Bradley Kissouna <uapv2500704@e-mc130-16.ad.univ-avignon.fr>
2024-11-08 17:38:14 +0100

    Change code

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -117,27 +117,28 @@ public class ListeSimple {
         return precedent;
     }
 
+
     public void echanger(Noeud r1, Noeud r2) {
-        if (r1 == r2)
+
+        if (r1 == r2 || r1 == null || r2 == null)
             return;
-        Noeud precedentR1, precedentR2;
-        if (r1 != tete && r2 != tete) {
-            precedentR1 = getPrecedent(r1);
-            precedentR2 = getPrecedent(r2);
-            precedentR1.setSuivant(r2);
-            precedentR2.setSuivant(r1);
-        } else if (r1 == tete) {
-            precedentR2 = getPrecedent(r2);
-            precedentR2.setSuivant(tete);
+
+        Noeud precedentR1 = r1 != tete ? getPrecedent(r1) : null;
+        Noeud precedentR2 = r2 != tete ? getPrecedent(r2) : null;
+
+        if (r1 == tete)
             tete = r2;
-        }
-        else if (r2 == tete) {
-            precedentR1 = getPrecedent(r1);
-            precedentR1.setSuivant(tete);
+        else if (r2 == tete)
             tete = r1;
-        }
-        Noeud temp = r2.getSuivant();
-        r2.setSuivant(r1.getSuivant());
-        r1.setSuivant(temp);
+    
+        if (precedentR1 != null)
+            precedentR1.setSuivant(r2);
+        
+        if (precedentR2 != null)
+            precedentR2.setSuivant(r1);
+    
+        Noeud temp = r1.getSuivant();
+        r1.setSuivant(r2.getSuivant());
+        r2.setSuivant(temp);
     }
 }
